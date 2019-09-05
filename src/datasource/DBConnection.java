1dd28d1d3885adce7cabcd574f6f2c0909df35d2
Zhengqing Zhu <zhuzhengqing321@qq.com>
2019-09-05 20:50:56 +1000

    no message

--- src/datasource/DBConnection.java
+++ src/datasource/DBConnection.java
@@ -3,11 +3,12 @@ package datasource;
 import java.sql.*;
 
 public class DBConnection {
-    
+    // local
     private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/bookshop";
     private static final String DB_USER = "postgres";
     private static final String DB_PASSWORD = "ZZQ930603";
     
+    // remote
 //    private static final String DB_CONNECTION = "jdbc:postgresql://ec2-54-221-214-3.compute-1.amazonaws.com:5432/d7dv19pab1v9m9";
 //    private static final String DB_USER = "xxaklhtmqaushi";
 //    private static final String DB_PASSWORD = "5604933d43705cf456fd16fece07dd6ab3e950b65c3e69d735783d3147b65bf7";
